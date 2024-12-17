b4978bf7e8e9b84561d26c1a3d68907a48e17c05
citrus <contact.citrus@protonmail.com>
2024-12-17 10:30:52 +0100

    Chatbot

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from openai import OpenAI
 import streamlit as st
-
+ 
 client = OpenAI()
 
-value = st.text_input("Prompt...")
-if (value):
-    txt = st.header("Waiting for api...")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "user","content": value}
-    ]
-    )
+def new_message(content: str):
+    with (st.chat_message("user")):
+        st.session_state.messages.append({"role":"user", "content": content})
+        st.write(content)
 
-    txt.text(completion.choices[0].message.content)
+    with (st.chat_message("assistant")):
+        field = st.text("waiting for an answer...")
+    
+        completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user","content": content}
+        ]
+        )
+        
+        st.session_state.messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+        field.text(completion.choices[0].message.content)
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+value = st.chat_input("Say something")
+if (value and value != ""):
+    new_message(value)
+    value = ""
