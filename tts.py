b7c54d2f7387752df7b61fdecd823fea4d69fed6
citrus <contact.citrus@protonmail.com>
2024-12-18 10:24:13 +0100

    Added audio

---
+++ tts.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import streamlit as st
+from pathlib import Path
+
+client = OpenAI()
+
+value = st.text_input("Votre prompt")
+
+if (value):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=value
+    )
+
+    file_path = Path(__file__).parent / "output.mp3"
+
+    response.stream_to_file(file_path)
+
+    st.audio("output.mp3", autoplay=True)
